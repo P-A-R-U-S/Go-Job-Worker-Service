5c3218f21f377a8cf7060ab3111491a1c864a0f6
Valentyn Ponomarenko <bootloader@list.ru>
2024-08-26 22:52:00 -0700

    - changed proto: JobCreateRequest.command changed to string type - minor implementation for jobWorkerServer.go

--- server/jobWorkerServer.go
+++ server/jobWorkerServer.go
@@ -6,6 +6,8 @@ import (
 	"github.com/google/uuid"
 	"google.golang.org/grpc"
 	"log"
+	"os"
+	"os/exec"
 )
 
 type JobWorkerServer struct {
@@ -18,6 +20,15 @@ func NewJobWorkerServer() *JobWorkerServer {
 
 func (s JobWorkerServer) Start(ctx context.Context, request *proto.JobCreateRequest) (*proto.JobResponse, error) {
 	jobUUID := uuid.New()
+
+	command := request.Command
+	arguments := request.Args
+	cmd := exec.Command(command, arguments...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Start()
+
 	log.Printf("new job with UUID: %s created\n", jobUUID)
 	return &proto.JobResponse{Uuid: jobUUID.String()}, nil
 }
@@ -35,6 +46,12 @@ func (s JobWorkerServer) Stream(request *proto.JobRequest, g grpc.ServerStreamin
 
 func (s JobWorkerServer) Stop(ctx context.Context, request *proto.JobRequest) (*proto.JobStatusResponse, error) {
 	jobUUID := uuid.New()
+
+	// Kill it:
+	//if err := cmd.Process.Kill(); err != nil {
+	//	log.Fatal("failed to kill process: ", err)
+	//}
+
 	log.Printf("new job with UUID: %s created\n", jobUUID)
 	return &proto.JobStatusResponse{Status: proto.Status_Stopped, ExitCode: -1, ExitReason: ""}, nil
 }
