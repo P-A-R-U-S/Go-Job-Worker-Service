fa92c5ae72509eb0c379adc3c053d15811d1ba92
Valentyn Ponomarenko <bootloader@list.ru>
2024-09-02 13:02:45 -0700

    - library: fixing Test_Job_Prevents_NetworkRequests

--- pkg/jobWorker/job.go
+++ pkg/jobWorker/job.go
@@ -147,25 +147,25 @@ func (job *Job) Start() error {
 			syscall.CLONE_NEWUTS |
 			syscall.CLONE_NEWPID |
 			syscall.CLONE_NEWUSER,
-		UidMappings: []syscall.SysProcIDMap{
-			{
-				ContainerID: 0,
-				HostID:      os.Getuid(),
-				Size:        1,
-			},
-		},
-		GidMappings: []syscall.SysProcIDMap{
-			{
-				ContainerID: 0,
-				HostID:      os.Getgid(),
-				Size:        1,
-			},
-		},
+		//UidMappings: []syscall.SysProcIDMap{
+		//	{
+		//		ContainerID: 0,
+		//		HostID:      os.Getuid(),
+		//		Size:        1,
+		//	},
+		//},
+		//GidMappings: []syscall.SysProcIDMap{
+		//	{
+		//		ContainerID: 0,
+		//		HostID:      os.Getgid(),
+		//		Size:        1,
+		//	},
+		//},
 		// Also, enables mounting a new proc filesystem so that command such as `ps -ef` only see the processes in the PID namespace
-		//Unshareflags: syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 		// instruct cmd.Run to use the control group file descriptor, so that Job Command does not
 		// have to manually add the new PID to the control group
-		//UseCgroupFD: true,
+		UseCgroupFD: true,
 	}
 
 	formatedUUID := strings.Replace(job.UUID.String(), "-", "", -1)
