e7e2af9157f061acf01b80ef1debad9c0d519b55
Valentyn Ponomarenko <bootloader@list.ru>
2024-09-03 22:16:59 -0700

    - library: update command for Test_Job_Running

--- pkg/jobWorker/job_test.go
+++ pkg/jobWorker/job_test.go
@@ -11,8 +11,8 @@ func Test_Job_Running(t *testing.T) {
 	//t.Parallel()
 
 	config := JobConfig{
-		Command:          "$(which date)",
-		Arguments:        []string{},
+		Command:          "echo",
+		Arguments:        []string{"$PATH"},
 		CPU:              0.5,           // half a CPU core
 		IOBytesPerSecond: 100_000_000,   // 100 MB/s
 		MemBytes:         1_000_000_000, // 1 GB
